9f26acb878e1d291953631d89af88e2803c688b1
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-12-22 20:37:54 +0700

    Thuan

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className='d-flex flex-column justify-content-center align-items-center w-100'>
        <div  style={{backgroundColor:"#f1f0f4"}} className='w-100 d-flex justify-content-center align-items-center'>
             <div className='w-75 d-flex justify-content-center align-items-center pt-3 pb-3'>
-                <h3 style={{letterSpacing:"-.5px",fontWeight:900,color:"#41444a"}}>For better experience,download <br /> the Swiggy app now</h3>
+                <h3 style={{letterSpacing:"-.5px",fontWeight:900,color:"#41444a"}}>For better experience,download <br /> the Thuan app now</h3>
                 <img className='img-fluid' width={'180px'} style={{height:"60px",marginLeft:"90px",marginRight:"30px"}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/play_store.png" alt="" />
                 <img width={'180px'} style={{height:"60px"}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/app_store.png" alt="" />
             </div>
@@ -15,7 +15,7 @@ function Footer() {
             <div className='w-75 d-flex justify-content-around  pt-5 pb-5'>
                 <div>
                     <div className='d-flex align-items-center'><img width={'30px'} src={logo} alt="Logo" />
-                    <h3 style={{color:"white"}} className='fw-bolder mt-1 ms-2'>Swiggy</h3></div>
+                    <h3 style={{color:"white"}} className='fw-bolder mt-1 ms-2'>Thuan</h3></div>
                     <p style={{color:"white"}}>© 2023 Bundl Technologies Pvt. Ltd</p>
                 </div>
                 <div style={{color:"#9e9e9e"}}>
@@ -23,9 +23,9 @@ function Footer() {
                     <p>About</p>
                     <p>Careers</p>
                     <p>Team</p>
-                    <p>Swiggy One</p>
-                    <p>Swiggy Instamart</p>
-                    <p>Swiggy Genie</p>
+                    <p>Thuan One</p>
+                    <p>Thuan Instamart</p>
+                    <p>Thuan Genie</p>
                 </div>
                 <div style={{color:"#9e9e9e"}}>
                     <h5 style={{color:"white"}}>Contact us</h5>
@@ -34,7 +34,7 @@ function Footer() {
                     <p>Ride with us</p>
                     <h5 style={{color:"white"}} className='mt-5'>Legal</h5>
                     <p>Terms & Conditions</p>
-                    <p>Cokkie Policy</p>
+                    <p>Cookie Policy</p>
                     <p>Privacy Policy</p>
                 </div>
                 <div style={{color:"#9e9e9e"}}>
